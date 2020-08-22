1168d1716d0c0cbafeaddddc6aec2f332a3fc222
Francisco Castillo <fjcasti1@asu.edu>
2020-08-21 23:25:52 -0700

    Inquiry check and Send Email

--- btre/settings.py
+++ btre/settings.py
@@ -145,3 +145,10 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'fjcastillocarrasco@gmail.com'
+EMAIL_HOST_PASSWORD = 'bgefiughjxnclzwv' # Have to generate a google app password
+EMAIL_USE_TLS = True
--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 from django.contrib import messages
 from .models import Contact
+
 
 # Create your views here.
 
@@ -18,15 +20,25 @@ def contact(request):
 
         # Check if user has made inquiry already
         if request.user.is_authenticated:
-            has_contacted = Contact.objects.all().filter(
-                user_id=user_id, listing_id=listing_id)
+            has_contacted = Contact.objects.all().filter(user_id=user_id, listing_id=listing_id)
             if has_contacted:
                 messages.error(
                     request, 'You have already made an inquiry for this listing')
                 return redirect('/listings/'+listing_id)
         contact = Contact(listing=listing, listing_id=listing_id,
-                          name=name, email=email, phone=phone, message=message, user_id=user_id)
+                        name=name, email=email, phone=phone, message=message, user_id=user_id)
+
+        # Save inquiry and send email
         contact.save()
+        send_mail(
+            'Property listing inquiry',
+            'There has been an inquiry for ' + listing +
+            '. Sign into the admin panel for more info',
+            'fjcastillocarrasco@gmail.com',
+            ['fjcastillocarrasco@gmail.com', realtor_email],
+            fail_silently=False
+        )
+
         messages.success(
             request, 'Your request has been submitted, a realtor will contact you soon')
     return redirect('/listings/'+listing_id)
