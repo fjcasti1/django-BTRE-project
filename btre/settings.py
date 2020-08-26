6ab5ca211329711bb634e883c45b092f75c143ea
Francisco Castillo <fjcasti1@asu.edu>
2020-08-25 21:18:42 -0700

    Add local_settings import

--- btre/settings.py
+++ btre/settings.py
@@ -152,3 +152,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'fjcastillocarrasco@gmail.com'
 EMAIL_HOST_PASSWORD = 'bgefiughjxnclzwv' # Have to generate a google app password
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
