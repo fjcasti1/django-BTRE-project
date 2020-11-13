aac3bc6dbaa0568c076f1c9351c201e216dc199b
Francisco Castillo <fjcasti1@asu.edu>
2020-11-12 19:23:07 -0700

    Back to sqlite3 trying to deploy

--- btre/settings.py
+++ btre/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -101,7 +101,13 @@ DATABASES = {
         # 'PORT': env('DB_PORT'),
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3')
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
